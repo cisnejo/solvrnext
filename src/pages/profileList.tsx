d131ce444e71506fcda62197f749fc0d9dfd32a5
cisnejo <cisnerosjoseph122@gmail.com>
2022-08-21 22:45:02 -0700

    joined tables Users to SharedInfoTag and changes profileList

--- src/pages/profileList.tsx
+++ src/pages/profileList.tsx
@@ -4,16 +4,27 @@ import { useEffect, useState } from "react";
 import { prisma } from "../db/client";
 import styled from "styled-components";
 
+interface UserInfo {
+  id:string;
+  name: string;
+  sharedinfotags: {
+    served: string;
+    description: string;
+    rating: number;
+    available:boolean;
+  };
+}
 export function ProfileList(users: any) {
   const [userProfiles, setUserProfiles] = useState({});
 
-  console.log(users);
-  const listings = users.users.map((user: any) => (
-    <div>
+  console.log(users)
+  const listings = users.users.map((user: UserInfo) => (
+    <div key = {user.id} >
       <div>Name : {user.name}</div>
-      <div>Users Helped: {user.served}</div>
-      <div>Description: {user.description}</div>
-      <div>Rating: {user.rating}</div>
+      <div>Users Helped: {user.sharedinfotags.served}</div>
+      <div>Description: {user.sharedinfotags.description}</div>
+      <div>Rating: {user.sharedinfotags.rating}</div>
+      <div>available: {user.sharedinfotags.available}</div>
     </div>
   ));
 
@@ -26,14 +37,10 @@ export function ProfileList(users: any) {
 }
 
 export const getServerSideProps = async () => {
-  const users = await prisma.sharedInfoTag.findMany({
-    where:{
-      user:{
-        name:"Joseph Cisneros"
-      }
-    }
-     
-   
+  const users = await prisma.user.findMany({
+    include: {
+      sharedinfotags: true,
+    },
   });
 
   return { props: { users } };
